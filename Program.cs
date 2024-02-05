2e2cf1b0dd6911757f93cac51547e380a3d2b652
DeveloperAbdumalik <dreamyoutobe1@gmail.com>
2024-02-05 22:34:26 +0300

    INFRA:Upload Projekt

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, Enter your name:");
+string name = Console.ReadLine();
+System.Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Your name {name} and age {age} Add list.");
+int agek = 3;
+int intdifferense = age - agek;
+System.Console.WriteLine(intdifferense);
