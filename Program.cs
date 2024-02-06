105b0ba9182d127e5c0c744b0250cbcede9ef1ae
DeveloperAbdumalik <dreamyoutobe1@gmail.com>
2024-02-07 01:43:50 +0300

    learning metod

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
-﻿Console.WriteLine("Hello, Enter your name:");
-string name = Console.ReadLine();
-System.Console.WriteLine("Enter your age:");
-int age = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"Your name {name} and age {age} Add list.");
-int agek = 3;
-int intdifferense = age - agek;
-System.Console.WriteLine($"KESCHA's age is {intdifferense} years different from yours");
+﻿using KESCHA.Classes;
 
-if(age>agek)
-{
-    System.Console.WriteLine("You are old:");
-}
-else if(age==agek)
-{
-    System.Console.WriteLine("You are the same age:");
-}
-else
-{
-System.Console.WriteLine("You are young:");
-}
+Animal kescha = new Animal(name: "kescha", age: 3);
+
+kescha.Password();
+
+Console.WriteLine("Hello, Enter your name:");
+string userName = Console.ReadLine();
+kescha.Greet(userName);
+
+System.Console.WriteLine("Enter your age:");
+int userAge = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Your name {userName} and age {userAge} Add list.");
+
+ 
+
+kescha.CalculateAgeDifferenseNotReturn(userAge);
+kescha.PrintAgeDifference();
+kescha.CompareAges(userAge);
+kescha.TellAbautFriend(userName,userAge);
