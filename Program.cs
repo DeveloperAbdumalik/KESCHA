76bfc1f335e5b25cd13d55f2e0c227b0160c2b04
DeveloperAbdumalik <dreamyoutobe1@gmail.com>
2024-02-06 11:01:25 +0300

    teach if

--- Program.cs
+++ Program.cs
@@ -5,4 +5,17 @@ int age = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"Your name {name} and age {age} Add list.");
 int agek = 3;
 int intdifferense = age - agek;
-System.Console.WriteLine(intdifferense);
+System.Console.WriteLine($"KESCHA's age is {intdifferense} years different from yours");
+
+if(age>agek)
+{
+    System.Console.WriteLine("You are old:");
+}
+else if(age==agek)
+{
+    System.Console.WriteLine("You are the same age:");
+}
+else
+{
+System.Console.WriteLine("You are young:");
+}
